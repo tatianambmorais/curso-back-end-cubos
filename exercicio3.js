d0e2e795a51056f5aaed7dd04fcaa096b3421c50
tatianambmorais <tatianambmorais@gmail.com>
2023-07-05 17:16:19 -0300

    subindo exercicio 3

---
+++ exercicio3.js
@@ -0,0 +1,6 @@
+let valorDoTenis=129.99
+let valorQueTenho=80
+let descontoEmReal=(valorDoTenis)-(valorQueTenho)
+let descontoEmPorcentagem=(descontoEmReal*100)/valorDoTenis
+console.log(descontoEmPorcentagem)
+
