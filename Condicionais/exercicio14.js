95d821c63ed144066ceb67fb003dd7440305cb40
tatianambmorais <tatianambmorais@gmail.com>
2023-07-06 19:55:11 -0300

    subindo exercicio 14

---
+++ Condicionais/exercicio14.js
@@ -0,0 +1,9 @@
+const quantidadeDeAguaIngerida=0
+
+if (quantidadeDeAguaIngerida<1.5){
+	console.log ("Risco Alto - Você está ingerindo pouquissima água, beba mais água!");
+} else if (quantidadeDeAguaIngerida>1.5 && quantidadeDeAguaIngerida<3) {
+	console.log("Risco Moderado - Você está ingerindo pouca água, beba mais!");
+} else if (quantidadeDeAguaIngerida>3){
+	console.log("Risco Baixo - Você está ingerindo uma boa quantidade de água, parabéns!");
+}
