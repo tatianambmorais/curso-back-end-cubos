7055e0ee5cb0dcb67aaa0206bbe31472e704591e
tatianambmorais <tatianambmorais@gmail.com>
2023-07-06 18:02:51 -0300

    subindo exercicio 12

---
+++ Condicionais/exercicio12.js
@@ -0,0 +1,17 @@
+const diaDaSemana=7
+
+if (diaDaSemana===1){
+	console.log ("Segunda-feira");	
+} else if (diaDaSemana===2){
+	console.log ("Terça-feira");	
+} else if (diaDaSemana===3){
+	console.log ("Quarta-feira");	
+} else if (diaDaSemana===4){
+	console.log ("Quinta-feira");	 
+} else if (diaDaSemana===5){
+	console.log ("Sexta-feira");	
+} else if (diaDaSemana===6){
+		console.log ("Sabado");	
+	} else if (diaDaSemana===7){
+		console.log ("Domingo");	
+	}
