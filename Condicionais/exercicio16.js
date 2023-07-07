f90ccce883fc7d145b213843a7f5b67dc1c952b8
tatianambmorais <tatianambmorais@gmail.com>
2023-07-06 21:57:17 -0300

    adicionando exercicio 16

---
+++ Condicionais/exercicio16.js
@@ -0,0 +1,4 @@
+const idadeDoAluno=15;
+const acompanhadoDosPais=true;
+
+(idadeDoAluno>=18 || (acompanhadoDosPais)) ? console.log ("Rematrícula realizada com sucesso") : console.log("Não é possível efetuar matricula")
