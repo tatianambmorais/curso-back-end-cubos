ee968ea24b9e0087f006ee31f1b9f5e209722105
tatianambmorais <tatianambmorais@gmail.com>
2023-07-10 19:19:56 -0300

    correções

--- Condicionais/exercicio1.js
+++ Condicionais/exercicio1.js
@@ -1,10 +1,29 @@
-const jogada1 = "pedra";
+const jogada1 = "papel";
 const jogada2 = "tesoura";
 
 if (jogada1 === jogada2) {
   console.log("Empate");
-} else if (jogada1 === "pedra" && jogada2 === "tesoura") {
-  console.log(jogada1);
-} else {
-  console.log(jogada2);
+} else 
+if (jogada1 === "pedra")  {
+	if (jogada2==="tesoura") {
+		console.log ("Pedra ganhou");
+	} else {
+		console.log ("Papel ganhou")
+	}
+
+} else if (jogada1==="tesoura") {
+	if (jogada2==="pedra"){
+		console.log("Pedra ganhou")
+	if (jogada2==="papel"){
+		console.log("Tesoura ganhou")
+	}
+	}
+  
+} else { 
+	if (jogada2==="tesoura"){
+		console.log("Tesoura venceu")
+	} else {
+		console.log ("Papel venceu")
+	}
+
 }
--- exercicio8.js
+++ exercicio8.js
@@ -1,4 +1,4 @@
-R=1
+	R=1
 C=2*Math.PI*R
 console.log (`A circunferência do círculo é de ${C}`)
 A=Math.PI*Math.pow(R,2)
