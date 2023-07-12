f4c6d94792acd90b147b7af3c2565c879ec84e13
tatianambmorais <tatianambmorais@gmail.com>
2023-07-12 12:35:39 -0300

    subindo exercicio 3

---
+++ objetos/exercicio3.js
@@ -0,0 +1,9 @@
+const usuario = {
+    nome: "João",
+    idade: 25,
+    profissao: "programador",
+    cor_preferida: "verde",
+}
+const {nome, idade, profissao, cor_preferida}=usuario;
+// Olá... Eu sou João e tenho 25 anos de idade. Sou programador e minha cor preferida é verde!
+console.log(`Olá, eu sou ${nome} e tenho ${idade} anos de idade. Sou ${profissao} e minha cor preferida é ${cor_preferida}!`)
