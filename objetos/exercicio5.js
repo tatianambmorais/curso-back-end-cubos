1e8acc37d52007ad913e5f792570199d19b24317
tatianambmorais <tatianambmorais@gmail.com>
2023-07-12 16:40:38 -0300

    subindo exercicios 4 e 5

---
+++ objetos/exercicio5.js
@@ -0,0 +1,32 @@
+const curso = {
+    id: 1234,
+    nome: "Lógica de programação",
+    aulas: [],
+}
+
+const aula1={
+	Identificador: 1,
+	Nome: "Introdução a programação",
+
+}
+
+const aula2={
+	Identificador: 2,
+	Nome: "Variáveis",
+
+}
+const aula3={
+	Identificador: 3,
+	Nome: "Condicionais",
+
+}
+const aula4={
+	Identificador: 4,
+	Nome: "Arrays",
+
+}
+curso.aulas.push(aula1);
+curso.aulas.push(aula2);
+curso.aulas.push(aula3);
+curso.aulas.push(aula4);
+console.log (curso)
