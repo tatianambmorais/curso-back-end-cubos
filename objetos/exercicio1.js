a6ba525601eda1e99539b090fbff307d420723c6
tatianambmorais <tatianambmorais@gmail.com>
2023-07-12 12:17:37 -0300

    subindo exercicio 1

---
+++ objetos/exercicio1.js
@@ -0,0 +1,21 @@
+const endereco= {
+	rua: "rua dos Bobos",
+	numero: 0,
+	complemento: "",
+	bairro: "laranjeiras satisfeito sorri",
+	cep: 58258150,
+	cidade: "felizlândia",
+	estado: "california",
+	pais: "cuboslândia"
+
+}
+
+const usuario = {
+	nome: "Juninho",
+	email: "juninho@gmail.com",
+	telefone: 8399000000,
+	data_nascimento: "04/07/2000",
+	endereco
+
+}
+console.log (usuario)
---
+++ objetos/provisório.js
@@ -0,0 +1,10 @@
+let produtos={
+	nome: ["Gin", "Rapadura", "Coxinha"],
+	preço: [10000, 5000, 7000],
+	quantidade: [10,5,7]}
+
+let cartao= {nome: "José",
+  idade: "30 anos",
+produtos}
+
+  console.log (cartao)
