f4fd90456b296a4ad1c941ac068dd9cd299d5080
tatianambmorais <tatianambmorais@gmail.com>
2023-07-12 13:26:49 -0300

    subindo exercicio 4

---
+++ objetos/exercicio4.js
@@ -0,0 +1,33 @@
+const usuarios = [
+	{
+	  nome: "João",
+	  idade: 25,
+	},
+	{
+	  nome: "Ana",
+	  idade: 18,
+	},
+	{
+	  nome: "Beatriz",
+	  idade: 15,
+	},
+	{
+	  nome: "Carlos",
+	  idade: 16,
+	},
+	{
+	  nome: "Antonio",
+	  idade: 32,
+	},
+  ];
+  
+  for (let i = 0; i < usuarios.length; i++) {
+	if (usuarios[i].idade > 17) {
+	  usuarios[i].maior_idade = true;
+	} else {
+	  usuarios[i].maior_idade = false;
+	}
+  }
+  
+  console.log(usuarios);
+  
