f240d3e1bbb28a82daedcef05fbb7ffde43debe7
tatianambmorais <tatianambmorais@gmail.com>
2023-07-13 14:14:01 -0300

    subindo exercicio 1

--- funcoes/exercicio1.js
+++ funcoes/exercicio1.js
@@ -1,1 +1,37 @@
-function corrigirProva(prova)
+const prova = {
+    aluno: "João",
+    materia: "Português",
+    valor: 10,
+    questoes: [
+        {
+            resposta: "a",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        },
+        {
+            resposta: "e",
+            correta: "b"
+        },
+        {
+            resposta: "b",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        }
+    ]
+}
+let somaDasQuestoesCorretas=0;
+for (i=0; i<prova.questoes.length; i++){
+	if (prova.questoes[i].resposta==prova.questoes[i].correta){
+		somaDasQuestoesCorretas++
+		}
+}	let nota=somaDasQuestoesCorretas*2
+function corrigirProva(prova){
+	console.log(`O aluno ${prova.aluno} acertou ${somaDasQuestoesCorretas} e tirou nota ${nota}`)
+}
+corrigirProva(prova)
