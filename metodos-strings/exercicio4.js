b88fd11207513451a2d20bca6799d7c6df8490d8
tatianambmorais <tatianambmorais@gmail.com>
2023-07-16 16:45:50 -0300

    subindo com alegria exercicio 4

---
+++ metodos-strings/exercicio4.js
@@ -0,0 +1,35 @@
+let identificador = "123";
+let nome = "José silva costa";
+let arrayDeNome=nome.split(" ");
+let email = "      jose@email.com  ";
+
+//O padrão do identificador são seis dígitos e caso o número não possua essa quantidade, 
+//complete com zero a esquerda; padStart
+//O nome e sobrenome precisam sempre começar com letra maiúscula; - toUpperCase
+//O e-mail não pode ter espaços em branco; trim
+//Para testar, use o console.log() para imprimir cada 
+
+function verificaIdentificador (){
+	if (identificador.length<6){
+		identificadorRefatorado=identificador.padStart(6,"0")
+	}
+} 
+verificaIdentificador()
+console.log(identificadorRefatorado)
+
+let nomeFormatado = "";
+function formataNome(){
+for (let item of arrayDeNome){
+	let primeiraLetra=item.slice(0,1);
+	let restanteDoNome=item.slice(1)
+
+	nomeFormatado+=primeiraLetra.toUpperCase() + restanteDoNome + " ";
+}}
+formataNome()
+console.log (nomeFormatado)
+
+const emailFormatado=email.trim()
+console.log(emailFormatado)
+
+
+
