737a5cc01dfdd7347a5e1d81e0479f9d320e7588
tatianambmorais <tatianambmorais@gmail.com>
2023-07-19 20:53:40 -0300

    corrigindo exercicio 12

--- Arrays/exercicio12.js
+++ Arrays/exercicio12.js
@@ -3,7 +3,7 @@ const filaDeDentro=["Jose", "Maria", "Joao"];
 const filaDeFora=["Joana", "Roberta", "Marcos", "Felipe"];
 // quando filaDeDentro.lenght<0, adicionar filaDeFora[0] para filaDeDento
 
-while (filaDeDentro.length<5)
+
 	if (filaDeDentro.length<5){
 	filaDeDentro.push(filaDeFora[0]);
 	filaDeFora.shift(); 
