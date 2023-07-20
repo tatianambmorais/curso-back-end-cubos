8bf23cf66a097c2145280f3c3c5b017be0f30135
tatianambmorais <tatianambmorais@gmail.com>
2023-07-20 13:58:20 -0300

    corrigindo exercicio 2

--- funcoes/exercicio2.js
+++ funcoes/exercicio2.js
@@ -1,30 +1,54 @@
-const carro={
-	ligado: false,
-	velocidade: 0,
-	verificaCarroLigado: function (){ligado? console.log("Este carro está ligado") : true;},
-	aceleraCarro: function (){	ligado? velocidade+=10 : console.log ("Não é possível acelerar um carro desligado")},
-	desaceleraCarro: function (){ligado? velocidade-=10 : console.log ("Não é possível desacelerar um carro desligado")},
-	 mostraStatus: function (){console.log (`Carro ${verificaCarroLigado}. Velocidade ${velocidade}`)},
+const carro = {
+    ligado: false,
+    velocidade: 0,
+    devolveStatusTexto: function () {
+        return this.ligado ? 'ligado' : 'desligado';
+    },
+    imprimeStatusEVelocidade: function () {
+        console.log(`Carro ${this.devolveStatusTexto()}. Velocidade: ${this.velocidade}.`);
+    },
+    ligar: function () {
+        if (this.ligado) {
+            console.log('Este carro já está ligado.');
+        } else {
+            this.ligado = true;
+            this.imprimeStatusEVelocidade();
+        }
+    },
+    desligar: function () {
+        if (!this.ligado) {
+            console.log('Este carro já está desligado.');
+        } else {
+            if (this.velocidade > 0) {
+                console.log('Não é possível desligar um carro em movimento.');
+            } else {
+                this.ligado = false;
+                this.velocidade = 0;
+                this.imprimeStatusEVelocidade();
+            }
+        }
+    },
+    acelerar: function () {
+        if (!this.ligado) {
+            this.ligado = true;
+        }
 
-	}
-
-	verificaCarroLigado(false)
-
-
-
-/*}
-
-function aceleraCarro(){
-	ligado? velocidade+=10 : console.log ("Não é possível acelerar um carro desligado")
+        this.velocidade += 10;
+        this.imprimeStatusEVelocidade();
+    },
+    desacelerar: function () {
+        if (!this.ligado) {
+            console.log('Não é possível desacelerar um carro desligado.');
+        } else {
+            this.velocidade -= 10;
+            if (this.velocidade === 0) {
+                this.ligado = false;
+            }
+            this.imprimeStatusEVelocidade();
+        }
+    },
 }
+	
 
-function desaceleraCarro(){
-	ligado? velocidade-=10 : console.log ("Não é possível desacelerar um carro desligado")
-}
-
-function mostraStatus(){
-	if {}
-	console.log (`Carro ${verificaCarroLigado}. Velocidade ${velocidade}`)
-}
-
-//monitoria*/
+carro.desligar();
+carro.ligar();
